52498da79b0930b5d635923f077357fe070b197e
Wilson Horrell <wilsonhorrell@gmai.com>
2019-03-23 23:31:46 +0000

    Finished button and linking form with page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,4 +8,14 @@ class ContactsController < ApplicationController
 	def new 
 		@contact = Contact.new
 	end
+
+	def create
+		Contact.create(contact_params)
+		redirect_to root_path
+
+	end
+
+	def contact_params
+		params.require(:contact).permit(:name, :description, :address)
+	end
 end
